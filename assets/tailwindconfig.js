c9efdef8d46b6203b2a24ea2431ae125b35054b7
Antonio Navarro <anavarro67@hotmail.com>
2025-03-13 19:32:00 +0100

    incluir header with totals

---
+++ assets/tailwindconfig.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          midnightBlue: "#25245E",
+          sandyOrange: "#FAC269",
+          aquaBlue: "#5ACEC7",
+          lightIceBlue: "#E5ECF6",
+          softSteelBlue: "#718EBF",
+          charcoalGray: "#494949",
+          silverGray: "#A4A4A4",
+          white: "#FFFFFF",
+          coralPink: "#FE5C73",
+          mintGreen: "#16DBAA",
+        },
+        boxShadow: {
+          'custom': '0px 4px 4px 0px #00000026',
+        },
+      },
+    },
+  };
+  
